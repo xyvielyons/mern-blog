a3cad896ff0c74b6a3db9bc19869fd9f5f921e32
Twisted-shadow <111194732+Twisted-shadow@users.noreply.github.com>
2024-02-02 19:19:21 +0300

    create pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,25 @@
 import React from 'react'
-
+import {BrowserRouter,Routes,Route} from "react-router-dom"
+import Home from './pages/Home'
+import About from './pages/About'
+import Projects from './pages/Projects'
+import Signin from "./pages/Signin"
+import Signup from "./pages/Signup"
+import Dashboard from "./pages/Dashboard" 
 export default function App() {
   return (
-    <h1 className='text-3xl text-red-500'>App</h1>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home></Home>}></Route>
+      <Route path="/about" element={<About></About>}></Route>
+      <Route path="/projects" element={<Projects></Projects>}></Route>
+      <Route path="/sign-in" element={<Signin></Signin>}></Route>
+      <Route path="/sign-up" element={<Signup></Signup>}></Route>
+      <Route path="/dashboard" element={<Dashboard></Dashboard>}></Route>
+  
+
+    </Routes>
+    
+    </BrowserRouter>
   )
 }
