76c28bea16dfdbeda47882ff6a21f37b17022c6b
Twisted-shadow <111194732+Twisted-shadow@users.noreply.github.com>
2024-02-02 21:33:11 +0300

    updated version of server

--- api/index.js
+++ api/index.js
@@ -5,7 +5,13 @@ import mongoose from "mongoose"
 import dotenv from "dotenv"
 
 
-dotenv.config({path:"./config.env"})
+dotenv.config()
+process.on("uncaughtException",(err)=>{
+    console.log(err.message)
+    console.log("unhandled exception occured application shutting down")
+    process.exit(1)
+})
+
 
 const app = express();//running express js
 app.use(bodyParser.json({limit:"30mb",extended:true}))
@@ -13,10 +19,12 @@ app.use(bodyParser.urlencoded({limit:"30mb",extended:true}))
 app.use(cors())//cross origin 
 
 
+
+
 const PORT = process.env.PORT
 mongoose.connect(process.env.CONN_STR,{
     useNewUrlParser:true,
     useUnifiedTopology:true
 })
-.then(()=>app.listen(PORT,()=>console.log(`server running at port: ${PORT}`)))
-.catch((err)=>console.log(err.message))
+.then(()=>app.listen(PORT,()=>console.log(`db connectio successful server running at port: ${PORT}`)))
+
