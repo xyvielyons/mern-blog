f2ea28457ee25473b3920f00344da6c0950a7fdc
Twisted-shadow <111194732+Twisted-shadow@users.noreply.github.com>
2024-02-05 15:21:01 +0300

    dark mode functionality

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -3,9 +3,10 @@ import userReducer from "./user/userSlice.js"
 import {persistReducer} from "redux-persist"
 import storage from "redux-persist/lib/storage"
 import persistStore from "redux-persist/es/persistStore"
-
+import themeReducer from "./theme/themeSlice.js"
 const rootReducer = combineReducers({
     user:userReducer,
+    theme:themeReducer
 })
 const persistConfig = {
     key:"root",
