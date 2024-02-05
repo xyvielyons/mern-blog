40d69a6a18687859b0d16016dd10522302b18fee
Twisted-shadow <111194732+Twisted-shadow@users.noreply.github.com>
2024-02-05 18:45:22 +0300

    complete sidebar of the dashboard

---
+++ client/src/components/DashSidebar.jsx
@@ -0,0 +1,37 @@
+import React, { useEffect,useState } from 'react'
+import {Sidebar} from "flowbite-react"
+import {useLocation} from 'react-router-dom'
+import {HiUser,HiArrowSmRight} from "react-icons/hi"
+import { Link } from 'react-router-dom'
+export default function DashSidebar() {
+    const location = useLocation()
+    const [tab ,setTab] = useState("")
+
+  useEffect(()=>{
+    const urlParams = new URLSearchParams(location.search)
+    const tabFromUrl = urlParams.get('tap')
+    console.log(tabFromUrl,urlParams,location)
+    if(tabFromUrl){
+      setTab(tabFromUrl)
+    }
+  },[location.search])
+
+  return (
+    <Sidebar className='w-full md:w-56'>
+        <Sidebar.Items>
+            <Sidebar.ItemGroup>
+                <Link to="/dashboard?tap=profile">
+                <Sidebar.Item active={tab==="profile"} icon={HiUser} label={"User"} labelColor="dark">
+                  Profile
+                </Sidebar.Item>
+                </Link>
+                <Sidebar.Item  icon={HiArrowSmRight} className="cursor-pointer">
+                  Sign Out
+                </Sidebar.Item>
+                
+
+            </Sidebar.ItemGroup>
+        </Sidebar.Items>
+    </Sidebar>
+  )
+}
--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,7 +1,32 @@
-import React from 'react'
-
+import React, { useEffect,useState } from 'react'
+import {useLocation} from 'react-router-dom'
+import DashSidebar from '../components/DashSidebar'
+import DashProfile from '../components/DashProfile'
 export default function Dashboard() {
+  const [tab ,setTab] = useState("")
+  const location = useLocation()
+
+  useEffect(()=>{
+    const urlParams = new URLSearchParams(location.search)
+    const tabFromUrl = urlParams.get('tap')
+    console.log(tabFromUrl,urlParams,location)
+    if(tabFromUrl){
+      setTab(tabFromUrl)
+    }
+  },[location.search])
+
+  
+  
   return (
-    <div>Dashboard</div>
+    <div className="min-h-screen flex flex-col md:flex-row">
+
+      <div className="md:w-56">
+        <DashSidebar></DashSidebar>
+      </div>
+
+      {tab==="profile" && <DashProfile></DashProfile>}
+
+
+    </div>
   )
 }
