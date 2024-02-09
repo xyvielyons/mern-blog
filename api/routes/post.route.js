c63f864e0adf0549508f12f31ae48bc529493233
Twisted-shadow <111194732+Twisted-shadow@users.noreply.github.com>
2024-02-10 02:29:53 +0300

    create get posts api route

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,8 +1,10 @@
 import express from "express";
 import { verifyToken } from "../Utils/VerifyUser.js";
-import { create } from "../controllers/post.controller.js";
+import { create ,getPosts} from "../controllers/post.controller.js";
 const router = express.Router()
 router.route('/create')
 .post(verifyToken,create)
+router.route('/getPosts')
+.get(getPosts)
 
 export default router
