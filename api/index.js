76f3dd8c27787101fc5e794918c67258bc749b63
xyvielyons <111194732+xyvielyons@users.noreply.github.com>
2024-02-17 20:04:27 +0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -22,11 +22,37 @@ const __dirname = path.resolve();
 const app = express();//running express js
 app.use(bodyParser.json({limit:"30mb",extended:true}))
 app.use(bodyParser.urlencoded({limit:"30mb",extended:true}))
-app.use(cors({credentials:true,origin:["http://localhost:5173","https://www.google.com","https://xaviers-mern-blog.onrender.com","https://cloud.mongodb.com"]}))//cross origin 
+//app.use(cors({credentials:true,origin:["http://localhost:5173","https://www.google.com","https://xaviers-mern-blog.onrender.com","https://cloud.mongodb.com"]}))//cross origin 
 //app.use(cors())//cross origin 
 //app.use(cors({credentials:true,origin:"*"}))//cross origin 
+
 app.use(cookieParser())//extract cookie from browser
 
+const corsOptions = {
+  origin:"https://xaviers-mern-blog.onrender.com",
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",};
+app.use(cors(corsOptions));
+
+app.use((req, res, next) => {
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://xaviers-mern-blog.onrender.com"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Allow-Private-Network", true);
+  //  Firefox caps this at 24 hours (86400 seconds). Chromium (starting in v76) caps at 2 hours (7200 seconds). The default value is 5 seconds.
+  res.setHeader("Access-Control-Max-Age", 7200);
+
+  next();
+});
 
 const PORT = process.env.PORT
 mongoose.connect(process.env.CONN_STR,{
