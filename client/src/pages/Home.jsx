685e7abad9f05df6616906d55578bd9547c3c131
Twisted-shadow <111194732+Twisted-shadow@users.noreply.github.com>
2024-02-17 15:20:32 +0300

    complete the home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,8 +1,23 @@
-import React, { useEffect } from 'react'
+import React, { useEffect ,useState} from 'react'
 import authFetch from '../axios/custom'
 import {Link} from "react-router-dom"
 import CallToAction from "./../components/CallToAction"
+import {PostCard} from "./../components/PostCard"
 export default function Home() {
+
+  const [posts,setPosts] = useState([]);
+  useEffect(()=>{
+    const fetchPosts = async()=>{
+      const res = await authFetch.get('/post/getPosts')
+      if(res.data.status === "success"){
+        setPosts(res.data.posts)
+
+      }
+
+    }
+    fetchPosts()
+
+  },[])
 
 
   return (
@@ -18,6 +33,25 @@ export default function Home() {
 
       <div className="p-3 bg-amber-100 dark:bg-slate-700">
         <CallToAction/>
+      </div>
+
+      <div className="max-w-6xl mx-auto p-3 flex flex-col gap-8 py-7">
+        {
+          posts && posts.length > 0 && (
+            <div className="flex flex-col gap-6">
+              <h2 className='text-2xl font-semibold text-center'>Recent Posts</h2>
+              <div className="flex flex-wrap gap-4">
+                {posts.map((post)=>
+                <PostCard key={post._id} post={post}/>
+
+                )}
+              </div>
+              <Link to={'/search'} className='text-lg text-teal-500 hover:underline text-center'>
+              view all posts
+              </Link>
+            </div>
+          )
+        }
       </div>
 
     </div>
