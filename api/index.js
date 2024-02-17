2fdd689259d8203cb9d315d7506844e8142299d5
xyvielyons <111194732+xyvielyons@users.noreply.github.com>
2024-02-17 19:37:53 +0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -23,7 +23,8 @@ const app = express();//running express js
 app.use(bodyParser.json({limit:"30mb",extended:true}))
 app.use(bodyParser.urlencoded({limit:"30mb",extended:true}))
 //app.use(cors({credentials:true,origin:["http://localhost:5173","https://www.google.com"]}))//cross origin 
-app.use(cors())//cross origin 
+//app.use(cors())//cross origin 
+app.use(cors({credentials:true,origin:"*"]}))//cross origin 
 app.use(cookieParser())//extract cookie from browser
 
 
